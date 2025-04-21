c07986e8d01fa43d8ba6f4aaa7d54d652b05863d
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-21 21:54:50 +0530

    adding redux header footer componenets

--- react_project03/src/App.jsx
+++ react_project03/src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
+import { Header,Footer } from './components'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
   
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=> setLoading(false))
+  })
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline justify-center p-70">
-           Hello world!
-      </h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/>
+      <main>
+        TODO:{/* {outlet} */}
+      </main>
+      <Footer/>
+      </div>
+    </div>
+  ) :null;
+  
 }
 
 export default App
