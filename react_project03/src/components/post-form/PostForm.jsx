c505f7bea4caa36cb805f2044c5d44dad7f8594b
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-22 23:32:05 +0530

    fixing typo

--- react_project03/src/components/post-form/PostForm.jsx
+++ react_project03/src/components/post-form/PostForm.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react'
 import { useForm } from 'react-hook-form'
-import {Buttom,Input,Select, RTE} from '../index'
+import {Button,Input,Select, RTE} from '../index'
 import appwriteService from '../../appwrite/config'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
