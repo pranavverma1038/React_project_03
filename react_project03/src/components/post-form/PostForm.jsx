04f7175a1be12ee35e887fc527491c7db176e6b9
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-24 02:30:57 +0530

    fixing slugs

--- react_project03/src/components/post-form/PostForm.jsx
+++ react_project03/src/components/post-form/PostForm.jsx
@@ -58,7 +58,7 @@ function PostForm({post}) {
             return value
             .trim()
             .toLowerCase()
-            .replace(/^[a-zA-Z\d\s]+/g,'-')
+            .replace(/[^a-zA-Z\d\s]+/g,'-')
             .replace(/\s/g, '-')
         }
         return ''
