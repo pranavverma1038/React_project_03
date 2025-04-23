db78d45569e7f14a10bb6203c339f8a3f126e5bb
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-24 02:09:46 +0530

    adding key for tiny

--- react_project03/src/components/RTE.jsx
+++ react_project03/src/components/RTE.jsx
@@ -11,6 +11,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+          apiKey="fgrbwtcisvlvt4uc9o8ao2063p4ajsoberpjm502ltmbuvt1"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
